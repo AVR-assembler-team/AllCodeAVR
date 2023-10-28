fb189514f788e51499bfc3ac71ffea04c3073e9b
HoStWgSp <hostwg@vk.com>
2023-10-28 17:09:27 +1100

    Переделал команды очистки дисплея и записи симфолов с регистра и по ASCII

--- AVR/AVR.asm
+++ AVR/AVR.asm
@@ -12,3 +12,21 @@ Proga:
 // Âארא ןנמדנאללא
 	
 rjmp Proga
+
+Button0:
+	READ_REG PINB, r16
+	sbrc r16, 2
+		_jump CommandCreateAndS
+		_jump CommandCreateAnd
+ret
+
+CommandCreateAndS:
+	ldi r16, $40
+	HD44780U_Write_Register r16
+	//HD44780U_Write_Symbol 'a'
+	//HD44780U_Set_Cursor 1, 3
+ret
+CommandCreateAnd:
+	HD44780U_LCD_Clear
+ret
+
