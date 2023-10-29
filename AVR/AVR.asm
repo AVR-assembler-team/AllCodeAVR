dbe01c49aa8a7b26513076d7af6d9f9248d869f6
HoStWgSp <hostwg@vk.com>
2023-10-29 14:28:38 +1100

    Закончмл с перепиской модуля HD44780U

--- AVR/AVR.asm
+++ AVR/AVR.asm
@@ -13,22 +13,4 @@ Proga:
 	
 rjmp Proga
 
-Button0:
-	READ_REG PINB, r16
-	sbrc r16, 2
-		_jump CommandCreateAndS
-		_jump CommandCreateAnd
-ret
-
-CommandCreateAndS:
-	ldi r16, $40
-	//HD44780U_Write_Register r16
-	HD44780U_LCD_LED Off
-	//HD44780U_Write_Symbol 'a'
-	//HD44780U_Set_Cursor 1, 3
-ret
-CommandCreateAnd:
-	//HD44780U_LCD_Clear
-	HD44780U_LCD_LED On
-ret
-
+.include "Tests2.inc"
